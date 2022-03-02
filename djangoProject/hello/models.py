836d0558720ade17adf999e4769074286bb88d83
kangmin5 <mk@kangmin.kr>
2022-03-02 11:58:52 +0900

    2022-03-02파이썬으로 계산기,성적,BMI 만들기

--- djangoProject/hello/models.py
+++ djangoProject/hello/models.py
@@ -18,10 +18,37 @@ class Calcurator(object):
         return self.num1 / self.num2
 
 
+class Grade(object):
+    def __init__(self,name,kor,eng,math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+    def sum(self):
+        return self.kor + self.eng + self.math
+
+    def avg(self):
+        return (self.kor + self.eng + self.math) / 3
+
+class Bmi(object):
+
+    def __init__(self, name, height, weight):
+
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+    def bmi(self):
+        bmi = (weight / (height ** 2)) * 10000
+
+        return bmi
+
+
 if __name__ == '__main__':
 
     while 1:
-        menu = input('0.Exit 1.계산기(+-*/)')
+        menu = input('0.Exit 1.계산기(+-*/) 2.Bmi 3.Grade')
         if menu == 0:
             break
         elif menu == '1':
@@ -43,3 +70,31 @@ if __name__ == '__main__':
             print('*' * 30)
             print(f'{calc.num1}{opcode}{calc.num2}={result}')
             print('*' * 30)
+
+        elif menu == '2':
+            name: str = input('이름 입력')
+            height = float(input('키 입력'))
+            weight = float(input('몸무게 입력'))
+
+            bmi = Bmi(name, height, weight)
+            status =''
+
+            if bmi.bmi() >= 30.0:
+                status = '비만'
+            elif 25.0 <= bmi.bmi() < 30.0:
+                status = '과체중'
+            elif 18.5 <= bmi.bmi() < 25.0:
+                status = '정상'
+            else:
+                status = '저체중'
+
+            print(f'{name}님의 키는{height}이고 몸무게는{weight}이고 BMI는 {bmi.bmi()} 이고 {status} 입니다..')
+
+        elif '3':
+            name = input('이름 : ')
+            kor  = int(input('국어 : '))
+            eng  = int(input('영어 : '))
+            math  = int(input('수학 : '))
+            grade = Grade(name,kor,eng,math)
+
+            print(f'{name}님의 국어{kor} 영어{eng} 수학{math} 이고 합계는{grade.sum()} 이고 평균은{grade.avg()} 입니다.')
