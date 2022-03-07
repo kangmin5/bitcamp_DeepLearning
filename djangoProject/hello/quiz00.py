5688dcfb34bd634a36a9ab11d0d21981a8dc7b5d
kangmin5 <mk@kangmin.kr>
2022-03-07 17:50:17 +0900

    2022-03-07 파이썬 알고리즘 titanic을 위한 account실습(list구현,enumerate)

--- djangoProject/hello/quiz00.py
+++ djangoProject/hello/quiz00.py
@@ -4,21 +4,25 @@ from hello.domains import my100, myRandom, Member, members
 
 
 class Quiz00:
-    def quiz00calculator(self)-> float:
-        a = random.randint(1,100)
-        b = random.randint(1,100)
-        op=['+','-','*','/','%']
-
-        res=op[random.randint(0,4)]
-        if(res=='+'):  print(f'{a}+{b}={a+b}')
-        elif(res=='-'):  print(f'{a}-{b}={a-b}')
-        elif(res =='*'):  print(f'{a}*{b}={a*b}')
-        elif(res=='/'):  print(f'{a}/{b}={a/b}')
-        elif(res=='%'):  print(f'{a}%{b}={a%b}')
+    def quiz00calculator(self) -> float:
+        a = random.randint(1, 100)
+        b = random.randint(1, 100)
+        op = ['+', '-', '*', '/', '%']
+        res = op[random.randint(0, 4)]  # 인덱싱 (5가지중의 1개를 무작위로 뽑아낸다)
+        if (res == '+'):
+            print(f'{a}+{b}={a + b}')
+        elif (res == '-'):
+            print(f'{a}-{b}={a - b}')
+        elif (res == '*'):
+            print(f'{a}*{b}={a * b}')
+        elif (res == '/'):
+            print(f'{a}/{b}={a / b}')
+        elif (res == '%'):
+            print(f'{a}%{b}={a % b}')
 
         return None
 
-# ---------------------------------------------------
+    # ---------------------------------------------------
     def quiz01bmi(self):
         this = Member()
         this.name = '홍길동'
@@ -35,80 +39,146 @@ class Quiz00:
             print(f'저체중')
 
         return None
-# ----------------------------------------------------------
+
+    # ----------------------------------------------------------
     def quiz02dice(self):
         print(random.randint(1, 6))
         return None
-# ----------------------------------------------------------------
+
+    # ----------------------------------------------------------------
     def quiz03rps(self):
-        p=int(input('1.가위 2.바위 3.보'))
-        c=random.randint(1,3)
-        if c-p == 2 or c-p == -1:
+        p = int(input('1.가위 2.바위 3.보'))
+        c = random.randint(1, 3)
+        if c - p == 2 or c - p == -1:
             print(f'플레이어: {p}\n 컴퓨터:{c}\n 결과:(플레이어)승리')
         elif c - p == 1 or c - p == -2:
             print(f'플레이어: {p}\n 컴퓨터:{c}\n 결과:(플레이어)패배')
         elif c - p == 0:
             print(f'플레이어: {p}\n 컴퓨터:{c}\n 결과:(플레이어)무승부')
-        else : "break"
+        else:
+            "break"
 
         return None
-# ----------------------------------------------------------------
-    def quiz04leap(self):
-        year = int(input('년도입력:'))
-        if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
-            print('윤년입니다')
-        else:
-            print('평년입니다')
+
+    # ----------------------------------------------------------------
+    def quiz04leap(self):  # 윤년
+        '''
+            Java스타일
+            s = (y % 4 == 0) and (y % 100 != 0) or (y % 400 == 0) ? "윤년" :"평년"
+        '''
+        y = random.randint(2000, 2022)
+        s = "윤년" if (y % 4 == 0) and (y % 100 != 0) or (y % 400 == 0) else "평년"
+        print(f'{y}년은 {s}입니다.')
         return None
-# -------------------------------------------------------------
+
+    # -------------------------------------------------------------
     def quiz05grade(self):
-        kor = random.randint(0,100)
+        kor = random.randint(0, 100)
         eng = random.randint(0, 100)
         math = random.randint(0, 100)
-        sum = kor+eng+math
-        avg = (kor+eng+math) /3
-        if avg >= 90:grade = 'A'
-        elif avg >= 80:grade = 'B'
-        elif avg >= 70:grade = 'C'
-        elif avg >= 65:grade = 'D'
-        elif avg >= 60:grade = 'E'
+        sum = kor + eng + math
+        avg = (kor + eng + math) / 3
+        if avg >= 90:
+            grade = 'A'
+        elif avg >= 80:
+            grade = 'B'
+        elif avg >= 70:
+            grade = 'C'
+        elif avg >= 65:
+            grade = 'D'
+        elif avg >= 60:
+            grade = 'E'
         else:
             grade = 'F'
-            if grade == 'F':passChk = '불합격'
-            else:passChk = '합격'
-        print (f'합계{sum},평균{avg},등급{grade},합격{passChk} ')
+            if grade == 'F':
+                passChk = '불합격'
+            else:
+                passChk = '합격'
+        print(f'합계{sum},평균{avg},등급{grade},합격{passChk} ')
         return None
-# --------------------------------------------------------------
+
+    # --------------------------------------------------------------
     def quiz06memberChoice(self):
-        student = members()
-        res= student[(random.randint(0, 23))]
-        print(f'선택 학생은 {res}')
-        return None
-# -----------------------------------------------------------------
+        return members()[(random.randint(0, 23))]
+
+    # -----------------------------------------------------------------
     def quiz07lotto(self):
-        res = random.sample(range(1,46),6)
+        res = random.sample(range(1, 46), 6)
         print(f'로또 번호는 {res}')
         return None
-# -----------------------------------------------------------------
-    def quiz08bank(self, balance=0):  # 이름, 입금, 출금만 구현
-        name = str(input('이름 :'))
-        while 1 :
-            menu = int(input('0.종료 1. 입금 2. 출금'))
-            if menu == 0:
-                break
-            elif menu ==1:
-                save = int(input('입금액:'))
-                balance +=save
-                print(f'잔액은 {balance}')
-            elif menu ==2:
-                withdrawal = int(input('출금액:'))
-                balance -= withdrawal
-                print((f'잔액은 {balance}'))
-        return None
-# -----------------------------------------------------------------
+
+    # -----------------------------------------------------------------
+    def quiz08bank(self):  # 이름, 입금, 출금만 구현
+        Account.main()
+
+    # -----------------------------------------------------------------
     def quiz09gugudan(self):  # 책받침구구단
-        for y in range(1,10):
-            for x in range(2,10):
-                print(x,'x', y, '=', x*y, end='\t')
+        for y in range(1, 10):
+            for x in range(2, 10):
+                print(x, 'x', y, '=', x * y, end='\t')
             print()
         return None
+
+
+'''
+08번 문제 해결을 위한 클래스는 다음과 같다.
+[요구사항(RFP)]
+은행이름은 Bitbank
+입금자 이름(name), 계좌번호(account_number), 금액(money) 속성값으로 계좌를 생성한다.
+계좌번호는 3자리-2자리-6자리 형태로 랜텀하게 생성된다. 예제 : 123-12-123456
+금액은 100만원 ~ 999만원 사이로 랜덤하게 입금된다.
+'''
+
+
+class Account(object):
+    def __init__(self,name,account_number,money):
+        self.BANK_NAME = '비트은행'
+        self.name = Quiz00().quiz06memberChoice() if name == None else name
+        self.account_number = self.creat_account_number() if account_number == None else account_number
+        self.money = random.randint(100, 999) if money ==None else money
+
+    def to_string(self):
+        return f'은행 : {self.BANK_NAME},' \
+               f'입금자 : {self.name}' \
+               f'계좌번호 :{self.account_number} ' \
+               f'금액 : {self.money}만원'
+
+    def creat_account_number(self):
+        return "".join(["-" if i==3 or i==6  else str(random.randint(0, 9)) for i in range(13)])
+
+    def del_account(self,ls,account_number):
+        for i,j in enumerate(ls):                        # 처음 i 는 index  j는 element
+            if j.account_number ==account_number:
+                del ls[i]
+
+    @staticmethod
+    def main():
+        ls =[]
+        while 1:
+            menu = input('0.종료 1.개좌개설 2.계좌내용 3.입금 4. 출금 5.계좌해지')
+            if menu == '0':
+                break
+            if menu == '1':
+                acc = Account(None,None,None)
+                print(f'{acc.to_string()}...개설되었습니다.')
+                ls.append(acc)
+            elif menu == '2':
+                a = '\n'.join(i.to_string() for i in ls )
+                print(a)
+            elif menu == '3':
+                account_number = input('입금할 계좌번호')
+                deposit = input('입금액')
+                for i,j in enumerate(ls):
+                    if j.account_number == account_number:
+                        pass
+
+
+            elif menu == '4':
+                account_number = input('출금할 계좌번호')
+                money = input('출금액')
+                #추가코드완성
+            elif menu == '5':
+                account_number = input('탈퇴할 계좌번호')
+            else:
+                print('Wrong Number... Try Again')
+                continue
--- djangoProject/hello/quiz20.py
+++ djangoProject/hello/quiz20.py
@@ -8,9 +8,18 @@ class Quiz20:
     def quiz21tuple(self) -> str: return None
 
     def quiz22dict(self) -> str: return None
-
-    def quiz23listcom(self) -> str: return None
-
+#--------------------------------------------------------------
+    def quiz23listcom(self) -> str:
+        print('-----------legacy-----------')
+        a = []                                 # a = list() 와 같다.
+        for i in range(5):
+            a.append(i)
+        print(a)
+        print('--------comprehension----------')
+        a2 = [i for i in range(5)]            # 이걸 사용해야 한다. (일 순위)  리스트 컴프리핸션 (list comprehension)
+        print(a2)
+        return None
+#--------------------------------------------------------------
     def quiz24zip(self) -> str: return None
 
     def quiz25dictcom(self) -> str: return None
